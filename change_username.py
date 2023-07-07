d74cea2489680f7c3d697c5c0709c79e4eec0297
MeepleCat <105896584+MeepleCat@users.noreply.github.com>
2023-07-07 18:23:23 -0500

    Assorted changes
    - Added keep_alive.py, creates a web server to ping constantly
    - Added change_username.py, allows the user to change their username registered with the program
    - Updated main.py to support the addition of the other two files

---
+++ change_username.py
@@ -0,0 +1,40 @@
+from get_column import get_column
+
+
+def change_username(message, sheet):
+    number_of_users = int(sheet.acell("L1").value)
+
+    column_c = get_column("C", sheet, "digit")
+
+    column_d = get_column("D", sheet, "digit")
+
+    found_user = False
+    for i in range(number_of_users):
+        temp_id = str(column_c[i]) + str(column_d[i])
+        print(f"- temp_id: {temp_id}\nauthorID: {message.author.id}")
+        if str(temp_id) == str(message.author.id):
+            found_user = True
+            print("--------------- FOUND USER ---------------")
+
+    print(found_user)
+    if found_user:
+        content = [char for char in message.content]
+
+        spaces = 0
+        while spaces < 3:
+            if content[0] == " ":
+                spaces = spaces + 1
+
+            del (content[0])
+
+        name = ""
+        while len(content) > 0:
+            name = name + content[0]
+            del (content[0])
+
+        sheet.update_acell(f"B{number_of_users + 1}", name)
+
+        return f"Your usernames has been changed to \"{name}\" successfully."
+
+    else:
+        return "You must submit your username in order to change it."
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import os
 from discord import HTTPException
 
 from whitelist import whitelist
+from change_username import change_username
 from notify import notify
 from whitelist_instructions import whitelist_instructions
 from claimed_numbers import claimed_numbers
 from claim_number import claim_number
 from packager_instructions import packager_instructions
+from keep_alive import keep_alive
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -32,13 +34,13 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 pingStart = "@"
-previousMessages = ["", "", "", "", "", "", "", "", "", ""]
+previousMessagesUsernames = ["", "", "", "", "", "", "", "", "", ""]
+previousMessagesPackagerNumbers = ["", "", "", "", "", "", "", "", "", ""]
 
 
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
-    global previousMessages
 
 
 @client.event
@@ -49,21 +51,21 @@ async def on_message(message):
     print(f"----------\nserver: {message.guild}\nchannel: {message.channel}\nauthor: {message.author}\ncontent: "
           f"{message.content}")
 
-    # determines the previous messages
-    global previousMessages
-    for i in range(9):
-        previousMessages[9-i] = previousMessages[8-i]
-    previousMessages[0] = message.content
-
-    if message.author == client.user:
-        return
-
     # runs the code for messages in channels called "usernames"
     if str(message.channel) == "usernames":
+        # determines the previous messages in the "usernames" channel
+        global previousMessagesUsernames
+        for i in range(9):
+            previousMessagesUsernames[9 - i] = previousMessagesUsernames[8 - i]
+        previousMessagesUsernames[0] = message.content
+
         # runs the code for the Whitelist command
         if message_lower.startswith("!meeplebot whitelist"):
-            print(99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
             await message.channel.send(whitelist(message, sheet))
+
+        # runs the code to allow the user to change their username
+        if message_lower.startswith("!meeplebot change username"):
+            await message.channel.send(change_username(message, sheet))
 
         # runs the code for the Notify command
         if message_lower.startswith("!meeplebot notify"):
@@ -72,12 +74,18 @@ async def on_message(message):
 
         # reposts the instructions message
         try:
-            await message.channel.send(whitelist_instructions(previousMessages))
+            await message.channel.send(whitelist_instructions(previousMessagesUsernames))
         except HTTPException:
             return
 
     # runs the code for messages in channels called "packager-numbers"
     if str(message.channel) == "packager-numbers":
+        # determines the previous messages in the "packager-numbers" channel
+        global previousMessagesPackagerNumbers
+        for i in range(9):
+            previousMessagesPackagerNumbers[9 - i] = previousMessagesPackagerNumbers[8 - i]
+        previousMessagesPackagerNumbers[0] = message.content
+
         # shows the claimed numbers
         if message_lower.startswith("!meeplebot claimed numbers"):
             await message.channel.send(claimed_numbers(sheet))
@@ -88,7 +96,9 @@ async def on_message(message):
 
         # reposts the instructions message
         try:
-            await message.channel.send(packager_instructions(previousMessages))
+            await message.channel.send(packager_instructions(previousMessagesPackagerNumbers))
         except HTTPException:
             return
+
+keep_alive()
 client.run(os.environ['TOKEN'])
