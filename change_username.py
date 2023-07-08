5e300bdd9a8da21b2cab89747e1d31d45d70dbb6
MeepleCat <105896584+MeepleCat@users.noreply.github.com>
2023-07-07 22:42:58 -0500

    Fixed an issue where the username changed would not belong to the person trying to change their username

--- change_username.py
+++ change_username.py
@@ -9,11 +9,13 @@ def change_username(message, sheet):
     column_d = get_column("D", sheet, "digit")
 
     found_user = False
+    user_row = -1
     for i in range(number_of_users):
         temp_id = str(column_c[i]) + str(column_d[i])
         print(f"- temp_id: {temp_id}\nauthorID: {message.author.id}")
         if str(temp_id) == str(message.author.id):
             found_user = True
+            user_row = i
             print("--------------- FOUND USER ---------------")
 
     print(found_user)
@@ -32,7 +34,7 @@ def change_username(message, sheet):
             name = name + content[0]
             del (content[0])
 
-        sheet.update_acell(f"B{number_of_users + 1}", name)
+        sheet.update_acell(f"B{user_row + 2}", name)
 
         return f"Your usernames has been changed to \"{name}\" successfully."
 
